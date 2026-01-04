d3486d178e5d123bc632856f6df6ef8f64b5bba1
sunwangyang <sunwangyang@byteawake.com>
2026-01-04 14:57:20 +0800

    222

---
+++ src/swy-ui/components/input/index.ts
@@ -0,0 +1,6 @@
+import Input from './src/input.vue'
+
+export default Input
+
+export * from './src/input'
+export type { InputInstance } from './src/instance'
---
+++ src/swy-ui/components/input/style/index.ts
@@ -0,0 +1,2 @@
+import '@swy-ui/components/base/style'
+import '@swy-ui/theme-chalk/src/input.scss'
