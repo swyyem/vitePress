a4b1d32d5366e331dafdd1266dbd6e5c777b3506
sunwangyang <sunwangyang@byteawake.com>
2026-01-23 17:08:29 +0800

    1

--- src/swy-ui/components/tag/src/tag.vue
+++ src/swy-ui/components/tag/src/tag.vue
@@ -13,7 +13,11 @@
       :aria-label="'关闭标签'"
       @click.stop="handleClose"
     >
-      <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        viewBox="0 0 1024 1024"
+        xmlns="http://www.w3.org/2000/svg"
+        style="width: 1em; height: 1em; display: block"
+      >
         <path
           fill="currentColor"
           d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"
@@ -31,7 +35,11 @@
         :aria-label="'关闭标签'"
         @click.stop="handleClose"
       >
-        <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          viewBox="0 0 1024 1024"
+          xmlns="http://www.w3.org/2000/svg"
+          style="width: 1em; height: 1em; display: block"
+        >
           <path
             fill="currentColor"
             d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"
